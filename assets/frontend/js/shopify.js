2f6053a9e7172f1404f91239a850ab4e100cc8f7
davidhirtz <david.hirtz@me.com>
2022-08-24 14:37:23 +0200

    Update shopify.js

--- assets/frontend/js/shopify.js
+++ assets/frontend/js/shopify.js
@@ -1,5 +1,4 @@
 class Shopify {
-
     constructor(config) {
         const _ = this;
 
@@ -9,83 +8,71 @@ class Shopify {
 
         _.client = ShopifyBuy.buildClient(config);
 
+        _.setCheckout = (checkout) => {
+            if (checkout) {
+                _.checkout = checkout;
+                _.updateCheckout();
+            } else {
+                _.createCheckout();
+            }
+        }
+
+        _.createCheckout = () => {
+            _.client.checkout.create().then(function (checkout) {
+                localStorage.setItem(_.storageKey, checkout.id);
+                _.checkout = checkout;
+            });
+        }
+
+        _.updateCheckout = () => {
+            if (_.checkout) {
+                const itemCount = _.checkout.lineItems.length;
+
+                if (_.itemCount != itemCount) {
+                    _.itemCount = itemCount;
+                    _.onItemCountChange();
+                }
+
+                _.afterCheckoutUpdate();
+            }
+        }
+
+        _.addLineItem = (variantId, quantity = 1) => {
+            return _.client.checkout.addLineItems(_.checkout.id, [
+                {
+                    variantId: btoa('gid://shopify/ProductVariant/' + variantId),
+                    quantity: quantity
+                }
+            ]).then(_.setCheckout);
+        }
+
+        _.updateLineItem = (lineItemId, quantity) => {
+            return _.client.checkout.updateLineItems(_.checkout.id, [
+                {
+                    id: lineItemId,
+                    quantity: quantity
+                }
+            ]).then(_.setCheckout);
+        }
+
+        _.removeLineItem = (lineItemId) => {
+            return _.client.checkout.removeLineItems(_.checkout.id, [lineItemId]).then(_.setCheckout);
+        }
+
+        _.formatPrice = (price) => {
+            return parseFloat(price).toLocaleString(this.client.config.language || undefined, {minimumFractionDigits: 2})
+        }
+
+        _.onItemCountChange = () => {
+        }
+
+        _.afterCheckoutUpdate = () => {
+        }
+
         if (localStorage.getItem(_.storageKey)) {
             _.client.checkout.fetch(localStorage.getItem(_.storageKey)).then(_.setCheckout);
         } else {
             _.createCheckout();
         }
-    }
-
-    setCheckout(checkout) {
-        const _ = this;
-
-        if (checkout) {
-            _.checkout = checkout;
-            _.updateCheckout();
-        } else {
-            _.createCheckout();
-        }
-    }
-
-    createCheckout() {
-        const _ = this;
-
-        _.client.checkout.create().then(function (checkout) {
-            localStorage.setItem(_.storageKey, checkout.id);
-            _.checkout = checkout;
-        });
-    }
-
-    updateCheckout() {
-        const _ = this;
-
-        if (_.checkout) {
-            const itemCount = _.checkout.lineItems.length;
-
-            if (_.itemCount != itemCount) {
-                _.itemCount = itemCount;
-                _.onItemCountChange();
-            }
-            // } else {
-            //     setTimeout(_.updateCheckoutCount, 100);
-            _.afterCheckoutUpdate();
-        }
-    }
-
-    addLineItem(variantId, quantity = 1) {
-        const _ = this;
-
-        return _.client.checkout.addLineItems(_.checkout.id, [
-            {
-                variantId: btoa('gid://shopify/ProductVariant/' + variantId),
-                quantity: quantity
-            }
-        ]).then(_.setCheckout);
-    }
-
-    updateLineItem(lineItemId, quantity) {
-        const _ = this;
-
-        return _.client.checkout.updateLineItems(_.checkout.id, [
-            {
-                id: lineItemId,
-                quantity: quantity
-            }
-        ]).then(_.setCheckout);
-    }
-
-    removeLineItem(lineItemId) {
-        const _ = this;
-        return _.client.checkout.removeLineItems(_.checkout.id, [lineItemId]).then(_.setCheckout);
-    }
-
-    formatPrice(price) {
-        return parseFloat(price).toLocaleString(this.client.config.language || undefined, {minimumFractionDigits: 2})
-    }
-
-    onItemCountChange() {
-    }
-
-    afterCheckoutUpdate() {
     }
 }
