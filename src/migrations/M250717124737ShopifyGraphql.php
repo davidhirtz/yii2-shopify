5284944d9b20cb54a8129cf50873f231278dfac7
davidhirtz <david.hirtz@me.com>
2025-07-28 18:50:28 +0200

    Update M250717124737ShopifyGraphql.php
    Removed the index of the images, as this is not working with all supported databases...

--- src/migrations/M250717124737ShopifyGraphql.php
+++ src/migrations/M250717124737ShopifyGraphql.php
@@ -24,24 +24,6 @@ class M250717124737ShopifyGraphql extends Migration
         $this->dropPrimaryKey('id', ProductImage::tableName());
         $this->addPrimaryKey('pk', ProductImage::tableName(), ['id', 'product_id']);
 
-        $this->addForeignKey(
-            'product_image_id_ibfk',
-            Product::tableName(),
-            'image_id',
-            ProductImage::tableName(),
-            'id',
-            'SET NULL'
-        );
-
-        $this->addForeignKey(
-            'product_variant_image_id_ibfk',
-            ProductVariant::tableName(),
-            'image_id',
-            ProductImage::tableName(),
-            'id',
-            'SET NULL'
-        );
-
         $schema = $this->getDb()->getSchema()->getTableSchema(ProductVariant::tableName());
 
         if ($schema->getColumn('inventory_management')) {
