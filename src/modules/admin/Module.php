5e1af45bdc9dfddb5abdd4dc8bb722621e19e389
davidhirtz <david.hirtz@me.com>
2025-11-17 17:54:26 +0100

    Main menu

--- src/modules/admin/Module.php
+++ src/modules/admin/Module.php
@@ -8,9 +8,10 @@ use davidhirtz\yii2\shopify\models\Product;
 use davidhirtz\yii2\shopify\models\Webhook;
 use davidhirtz\yii2\shopify\modules\admin\controllers\ProductController;
 use davidhirtz\yii2\shopify\modules\admin\controllers\WebhookController;
+use davidhirtz\yii2\skeleton\modules\admin\config\DashboardItemConfig;
+use davidhirtz\yii2\skeleton\modules\admin\config\DashboardPanelConfig;
+use davidhirtz\yii2\skeleton\modules\admin\config\MainMenuItemConfig;
 use davidhirtz\yii2\skeleton\modules\admin\ModuleInterface;
-use davidhirtz\yii2\skeleton\widgets\panels\DashboardItem;
-use davidhirtz\yii2\skeleton\widgets\panels\DashboardPanel;
 use Yii;
 use yii\helpers\ArrayHelper;
 
@@ -19,17 +20,12 @@ use yii\helpers\ArrayHelper;
  */
 class Module extends \davidhirtz\yii2\skeleton\base\Module implements ModuleInterface
 {
-    /**
-     * @var string|null the module display name, defaults to "Products"
-     */
-    public ?string $name = null;
+
     public $defaultRoute = 'product';
 
     public function init(): void
     {
-        $this->name ??= Yii::t('shopify', 'Products');
         $this->controllerMap = ArrayHelper::merge($this->getCoreControllerMap(), $this->controllerMap);
-
         parent::init();
     }
 
@@ -50,16 +46,16 @@ class Module extends \davidhirtz\yii2\skeleton\base\Module implements ModuleInte
     public function getDashboardPanels(): array
     {
         return [
-            'shopify' => new DashboardPanel(
-                name: $this->name,
+            'shopify' => new DashboardPanelConfig(
+                name: $this->getName(),
                 items: [
-                    'products' => new DashboardItem(
+                    'products' => new DashboardItemConfig(
                         label: Yii::t('shopify', 'View Products'),
                         url: ['/admin/product/index'],
                         icon: 'tags',
                         roles: [Product::AUTH_PRODUCT_UPDATE],
                     ),
-                    'webhooks' => new DashboardItem(
+                    'webhooks' => new DashboardItemConfig(
                         label: Yii::t('shopify', 'View Webhooks'),
                         url: ['/admin/shopify-webhook/index'],
                         icon: 'satellite-dish',
@@ -70,19 +66,27 @@ class Module extends \davidhirtz\yii2\skeleton\base\Module implements ModuleInte
         ];
     }
 
-    public function getNavBarItems(): array
+    protected function getName(): string
+    {
+        return Yii::t('shopify', 'Products');
+    }
+
+    public function getMainMenuItems(): array
     {
         return [
-            'shopify' => [
-                'label' => $this->name ?: Yii::t('shopify', 'Products'),
-                'icon' => 'tags',
-                'url' => ['/admin/product/index'],
-                'active' => ['admin/product', 'admin/shopify-webhook'],
-                'roles' => [
+            'shopify' => new MainMenuItemConfig(
+                label: $this->getName(),
+                url: ['/admin/product/index'],
+                icon: 'tags',
+                roles: [
                     Product::AUTH_PRODUCT_UPDATE,
                     Webhook::AUTH_WEBHOOK_UPDATE,
                 ],
-            ],
+                routes: [
+                    'admin/product',
+                    'admin/shopify-webhook',
+                ],
+            ),
         ];
     }
 }
